df5f4e1fba006dbe0e41d93925a15e24d306b339
“[ivanzovko]” <[izovko123@gmail.com]>
2026-01-14 22:34:36 +0100

    fixed some responsive items

--- App/app/lib/blog-search-params.ts
+++
@@ -1,8 +0,0 @@
-import { parseAsInteger, createLoader } from "nuqs/server"
-
-export const blogSearchParams = {
-  page: parseAsInteger.withDefault(1),
-  userId: parseAsInteger.withDefault(0), // 0 means show all users
-}
-
-export const loadBlogSearchParams = createLoader(blogSearchParams)
